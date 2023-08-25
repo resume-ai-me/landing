521de6fb92fd8e9b3e5bc349f44074dafc065289
Jose M Delgado <jdelgado2002@gmail.com>
2023-08-25 17:25:00 -0500

    call to action

--- assets/js/contact.form.js
+++ assets/js/contact.form.js
@@ -12,49 +12,41 @@
 
 (function($) {
     'use strict';
-    // Get the form.
-    var form = $('#contact-form');
 
-    // Get the messages div.
-    var formMessages = $('#form-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function(e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-        // Serialize the form data.
-        var formData = $(form).serialize();
-
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        })
-        .done(function(response) {
+    //thank you for downloading popup
+    const form = document.getElementById("contact-form");
+    const formMessages = document.getElementById("form-messages");
+    const thankYouMessage = document.getElementById('thankYouMessage');
+    
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+    
+      const formData = new FormData(form);
+    
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", form.action);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
             // Make sure that the formMessages div has the 'success' class.
             $(formMessages).removeClass('error');
             $(formMessages).addClass('success');
-
-            // Set the message text.
-            $(formMessages).text(response);
-
+            thankYouMessage.setAttribute('aria-hidden', 'false');
+            formMessages.innerHTML = thankYouMessage.innerHTML;
             // Clear the form.
-            $('#name, #email, #phone, #website, #message').val('');
-        })
-        .fail(function(data) {
-            // Make sure that the formMessages div has the 'error' class.
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
-
-            // Set the message text.
-            if (data.responseText !== '') {
-                $(formMessages).text(data.responseText);
-            } else {
-                $(formMessages).text('Oops! An error occured and your message could not be sent.');
-            }
-        });
+            $('#name, #lastName, #email').val('');
+          } else {
+            formMessages.innerHTML = `<div class="alert alert-danger">Oops! An error occurred and your message could not be sent. Error code: ${xhr.status}</div>`;
+          }
+        }
+      };
+      xhr.onerror = function() {
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+        formMessages.innerHTML = `<div class="alert alert-danger">Oops! An error occurred and your message could not be sent. Please try again later.</div>`;
+      };
+      xhr.send(new URLSearchParams(formData).toString());
     });
 
 })(jQuery);
--- assets/js/main.js
+++ assets/js/main.js
@@ -533,36 +533,6 @@
             }
         });
     }
-
-    //thank you for downloading popup
-    const form = document.getElementById("contact-form");
-    const formMessages = document.getElementById("form-messages");
-    const thankYouMessage = document.getElementById('thankYouMessage');
-    
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-    
-      const formData = new FormData(form);
-    
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", form.action);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200) {
-            thankYouMessage.setAttribute('aria-hidden', 'false');
-            formMessages.innerHTML = thankYouMessage.innerHTML;
-            
-          } else {
-            formMessages.innerHTML = `<div class="alert alert-danger">Oops! An error occurred and your message could not be sent. Error code: ${xhr.status}</div>`;
-          }
-        }
-      };
-      xhr.onerror = function() {
-        formMessages.innerHTML = `<div class="alert alert-danger">Oops! An error occurred and your message could not be sent. Please try again later.</div>`;
-      };
-      xhr.send(new URLSearchParams(formData).toString());
-    });
     
     //preloader
           $(window).on( 'load', function() {
